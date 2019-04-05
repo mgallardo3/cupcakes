4daa46c3ac608dab678e34e1fdea224ca45f62fa
mgallardo3 <mgallardo3@mail.greenriver.edu>
2019-04-04 22:36:55 -0400

    Updated the form using an associative array to display flavor options

--- index.php
+++ index.php
@@ -13,24 +13,56 @@
     <title>Cupcakes</title>
 </head>
 <body>
+<?php
 
+$flavorsList= array('grasshopper'=>"The Grasshopper",'maple'=>"Whiskey Maple Bacon",'carrot'=>"Carrot Walnut",
+    'caramel'=>"Salted Caramel Cupcake",'velvet'=>"Red Velvet", 'lemon'=>'Lemon Drop', 'tiramisu '=>'Tiramisu');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+    $name = $_POST['name'];
+    $flavors = $_POST['flavors'];
+}
+//initialize errors array
+ $errors=array();
+
+if(empty($name))
+{
+    $errors []= "You forgot to enter your name.";
+}
+if(sizeof($flavors)< 1)
+{
+    $errors[] = "Pick at least one flavor";
+}
+if(empty($errors))
+{
+    echo "You order has being placed";
+}
+else
+{
+    foreach($errors as $errorMessage)
+    {
+        echo " - $errorMessage<br>\n";
+    }
+}
+
+?>
     <h1>Cupcake Fundraiser</h1>
     <form class="form-container" action=" " method="post">
         <label for="name">Name:</label><br>
         <input type="text" name="name" id="name" placeholder="Please enter your name"><br>
         <label for="flavors">Cupcake flavors:</label><br><br>
         <div name="flavors">
-            <input type="checkbox" name="flavors[]" value="grasshopper">The Grasshopper<br>
-            <input type="checkbox" name="flavors[]" value="Maple">Whiskey Maple Bacon<br>
-            <input type="checkbox" name="flavors[]" value="carrot">Carrot Walnut<br>
-            <input type="checkbox" name="flavors[]" value="caramel">Salted Caramel Cupcake<br>
-            <input type="checkbox" name="flavors[]" value="velvet">Red Velvet<br>
-            <input type="checkbox" name="flavors[]" value="lemon">Lemon Drop<br>
-            <input type="checkbox" name="flavors[]" value="tiramisu">Tiramisu<br>
+            <?php foreach($flavorsList as $flavor => $value)
+            {
+                echo " <input type='checkbox' name='flavors[]' value='$flavorsList[$flavor]'>$flavorsList[$flavor]<br>";
+            }
+            ?>
         </div>
         <button type="submit" class="btn btn-success btn-block">Place Order</button>
     </form>
+
 </body>
 </html>
-<?php
+
 
